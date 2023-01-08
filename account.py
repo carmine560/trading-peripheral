6a11be647424ba22e215477119c385da917f809b
carmine <40491810+carmine560@users.noreply.github.com>
2023-01-08 20:39:49 +0900

    Create sync-portfolio

---
+++ account.py
@@ -0,0 +1,66 @@
+import os
+
+def configure(service):
+    import configparser
+
+    from xdg import xdg_config_home
+    import gnupg
+
+    config = configparser.ConfigParser()
+    config['Common'] = {'url': 'URL',
+                        'user_id_xpath': 'USER_ID_XPATH',
+                        'user_password_xpath': 'USER_PASSWORD_XPATH',
+                        'login_xpath': 'LOGIN_XPATH'}
+    config['Account'] = {'user_id': 'USER_ID',
+                         'user_password': 'USER_PASSWORD'}
+    xdg_config_home = xdg_config_home()
+    config.path = os.path.join(xdg_config_home, service + '.ini.gpg')
+    gpg = gnupg.GPG()
+    if os.path.exists(config.path):
+        with open(config.path, 'rb') as f:
+            decrypted_data = gpg.decrypt_file(f)
+
+        config.read_string(str(decrypted_data))
+        return config
+    else:
+        import io
+        import sys
+
+        if not os.path.isdir(xdg_config_home):
+            try:
+                os.makedirs(xdg_config_home)
+            except OSError as e:
+                print(e)
+                sys.exit(1)
+
+        private_keys = gpg.list_keys(True)
+        fingerprint = private_keys[0]['fingerprint']
+        with io.StringIO() as s:
+            config.write(s)
+            s.seek(0)
+            gpg.encrypt(s.read(), fingerprint, output=config.path)
+            sys.exit()
+
+def login(config):
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.firefox.options import Options
+    from selenium.webdriver.firefox.service import Service
+    from webdriver_manager.firefox import GeckoDriverManager
+
+    service = Service(executable_path=GeckoDriverManager().install(),
+                      log_path=os.path.devnull)
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.get(config['Common']['url'])
+    user_id_input = driver.find_element(By.XPATH,
+                                        config['Common']['user_id_xpath'])
+    user_id_input.send_keys(config['Account']['user_id'])
+    user_password_input = \
+        driver.find_element(By.XPATH, config['Common']['user_password_xpath'])
+    user_password_input.send_keys(config['Account']['user_password'])
+    driver.find_element(By.XPATH, config['Common']['login_xpath']).click()
+    # FIXME
+    driver.implicitly_wait(5)
+    return driver
---
+++ sync_portfolio.py
@@ -0,0 +1,81 @@
+from selenium.webdriver.common.by import By
+
+# FIXME
+def interact_with_browser(driver):
+    click_element(driver, '//*[@id="link02M"]/ul/li[1]/a')
+
+    # https://site2.sbisec.co.jp/ETGate/?_ControlID=WPLETpfR001Control&_PageID=DefaultPID&_DataStoreID=DSWPLETpfR001Control&_ActionID=DefaultAID&getFlg=on
+    click_element(driver, '//a[text()="登録銘柄リストの追加・置き換え"]')
+
+    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy.do?
+    click_element(driver, '//*[@id="pfcopyinputForm"]/table/tbody/tr/td/div[3]/p/a')
+
+    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/sender.do
+    click_element(driver, '//*[@name="tool_from" and @value="3"]')
+    click_element(driver, '//input[@value="次へ"]')
+
+    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/sendercheck.do
+    click_element(driver, '//*[@name="tool_to_1" and @value="1"]')
+    click_element(driver, '//input[@value="次へ"]')
+
+    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/receivercheck.do
+    click_element(driver, '//*[@name="add_replace_tool_01" and @value="1_2"]')
+    click_element(driver, '//input[@value="確認画面へ"]')
+
+    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/selectcheck.do
+    click_element(driver, '//input[@value="指示実行"]')
+
+def click_element(driver, xpath):
+    driver.find_element(By.XPATH, xpath).click()
+
+def convert_to_yahoo_finance(portfolio, quotes):
+    import csv
+    import json
+
+    with open(portfolio) as f:
+        dictionary = json.load(f)
+
+    csvfile = open(quotes, 'w')
+    writer = csv.writer(csvfile)
+
+    header = ['Symbol', 'Current Price', 'Date', 'Time', 'Change', 'Open',
+              'High', 'Low', 'Volume', 'Trade Date', 'Purchase Price',
+              'Quantity', 'Commission', 'High Limit', 'Low Limit', 'Comment']
+    writer.writerow(header)
+
+    row = []
+    for i in range(len(header) - 1):
+        row.append('')
+        # FIXME
+    for item in dictionary['list'][0]['secList']:
+        writer.writerow([item['secCd'] + '.T'] + row)
+
+    csvfile.close()
+
+if __name__ == '__main__':
+    import os
+    import re
+
+    import account
+    import file_utilities
+
+    HYPERSBI2_ROOT = os.path.expandvars(r'$APPDATA\SBI Securities\HYPERSBI2')
+    identifier = ''
+    for f in os.listdir(HYPERSBI2_ROOT):
+        if re.match('^[0-9a-z]{32}$', f):
+            identifier = f
+
+    portfolio = ''
+    if identifier:
+        portfolio = os.path.join(HYPERSBI2_ROOT, identifier, 'portfolio.json')
+
+    BACKUP_ROOT = os.path.join(os.path.expanduser('~'),
+                               r'Dropbox\Documents\Trading\Backups')
+    file_utilities.backup_file(portfolio, BACKUP_ROOT)
+
+    driver = account.login(account.configure('SBI Securities'))
+    interact_with_browser(driver)
+    driver.quit()
+
+    quotes = os.path.join(os.path.expanduser('~'), 'Downloads', 'quotes.csv')
+    convert_to_yahoo_finance(portfolio, quotes)
