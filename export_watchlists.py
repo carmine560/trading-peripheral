e9d9282e6d5c1336bd25d58a8dac8d470ce540a7
carmine <40491810+carmine560@users.noreply.github.com>
2023-01-09 16:51:05 +0900

    Add configure

--- export_watchlists.py
+++ export_watchlists.py
@@ -1,34 +1,86 @@
+import os
 import sys
 
 from selenium.webdriver.common.by import By
 
-# FIXME
-def interact_with_browser(driver):
-    click_element(driver, '//*[@id="link02M"]/ul/li[1]/a')
+def main():
+    import re
 
-    # https://site2.sbisec.co.jp/ETGate/?_ControlID=WPLETpfR001Control&_PageID=DefaultPID&_DataStoreID=DSWPLETpfR001Control&_ActionID=DefaultAID&getFlg=on
-    click_element(driver, '//a[text()="登録銘柄リストの追加・置き換え"]')
+    import account
+    import file_utilities
 
-    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy.do?
-    click_element(driver, '//*[@id="pfcopyinputForm"]/table/tbody/tr/td/div[3]/p/a')
+    config = configure()
+    # with open(config.path, 'w', encoding='utf-8') as f:
+    #     config.write(f)
 
-    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/sender.do
-    click_element(driver, '//*[@name="tool_from" and @value="3"]')
-    click_element(driver, '//input[@value="次へ"]')
+    HYPERSBI2_ROOT = os.path.expandvars(r'$APPDATA\SBI Securities\HYPERSBI2')
+    identifier = ''
+    for f in os.listdir(HYPERSBI2_ROOT):
+        if re.match('^[0-9a-z]{32}$', f):
+            identifier = f
 
-    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/sendercheck.do
-    click_element(driver, '//*[@name="tool_to_1" and @value="1"]')
-    click_element(driver, '//input[@value="次へ"]')
+    if identifier:
+        portfolio = os.path.join(HYPERSBI2_ROOT, identifier, 'portfolio.json')
+    else:
+        print('unspecified identifier')
+        sys.exit(1)
 
-    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/receivercheck.do
-    click_element(driver, '//*[@name="add_replace_tool_01" and @value="1_2"]')
-    click_element(driver, '//input[@value="確認画面へ"]')
+    file_utilities.backup_file(portfolio,
+                               os.path.join(
+                                   os.path.expanduser('~'),
+                                   r'Dropbox\Documents\Trading\Backups'))
 
-    # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/selectcheck.do
-    click_element(driver, '//input[@value="指示実行"]')
+    driver = account.login(account.configure('SBI Securities'))
+    interact_with_browser(config, driver)
+    driver.quit()
 
-def click_element(driver, xpath):
-    driver.find_element(By.XPATH, xpath).click()
+    convert_to_yahoo_finance(portfolio, os.path.join(os.path.expanduser('~'),
+                                                     'Downloads'))
+
+def configure():
+    import configparser
+
+    config = configparser.ConfigParser()
+    config['SBI Securities'] = \
+        {'replace_website_with_hypersbi2':
+         [('click_element',
+           '//*[@id="link02M"]/ul/li[1]/a'),
+          # https://site2.sbisec.co.jp/ETGate/?_ControlID=WPLETpfR001Control&_PageID=DefaultPID&_DataStoreID=DSWPLETpfR001Control&_ActionID=DefaultAID&getFlg=on
+          ('click_element',
+           '//a[text()="登録銘柄リストの追加・置き換え"]'),
+          # https://site0.sbisec.co.jp/marble/portfolio/pfcopy.do?
+          ('click_element',
+           '//*[@id="pfcopyinputForm"]/table/tbody/tr/td/div[3]/p/a'),
+          # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/sender.do
+          ('click_element',
+           '//*[@name="tool_from" and @value="3"]'),
+          ('click_element',
+           '//input[@value="次へ"]'),
+          # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/sendercheck.do
+          ('click_element',
+           '//*[@name="tool_to_1" and @value="1"]'),
+          ('click_element',
+           '//input[@value="次へ"]'),
+          # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/receivercheck.do
+          ('click_element',
+           '//*[@name="add_replace_tool_01" and @value="1_2"]'),
+          ('click_element',
+           '//input[@value="確認画面へ"]'),
+          # https://site0.sbisec.co.jp/marble/portfolio/pfcopy/selectcheck.do
+          ('click_element',
+           '//input[@value="指示実行"]')]}
+    config['Yahoo Finance'] = {}
+    config.path = os.path.splitext(__file__)[0] + '.ini'
+    config.read(config.path, encoding='utf-8')
+    return config
+
+def interact_with_browser(config, driver):
+    commands = eval(config['SBI Securities']['replace_website_with_hypersbi2'])
+    for i in range(len(commands)):
+        command = commands[i][0]
+        arguments = commands[i][1]
+        if command == 'click_element':
+            driver.find_element(By.XPATH, arguments).click()
 
 def convert_to_yahoo_finance(portfolio, output_root):
     import csv
@@ -73,33 +125,4 @@ def convert_to_yahoo_finance(portfolio, output_root):
         csvfile.close()
 
 if __name__ == '__main__':
-    import os
-    import re
-
-    import account
-    import file_utilities
-
-    HYPERSBI2_ROOT = os.path.expandvars(r'$APPDATA\SBI Securities\HYPERSBI2')
-    identifier = ''
-    for f in os.listdir(HYPERSBI2_ROOT):
-        if re.match('^[0-9a-z]{32}$', f):
-            identifier = f
-
-    portfolio = ''
-    if identifier:
-        portfolio = os.path.join(HYPERSBI2_ROOT, identifier, 'portfolio.json')
-    else:
-        print('unspecified identifier')
-        sys.exit(1)
-
-    file_utilities.backup_file(portfolio,
-                               os.path.join(
-                                   os.path.expanduser('~'),
-                                   r'Dropbox\Documents\Trading\Backups'))
-
-    driver = account.login(account.configure('SBI Securities'))
-    interact_with_browser(driver)
-    driver.quit()
-
-    convert_to_yahoo_finance(portfolio, os.path.join(os.path.expanduser('~'),
-                                                     'Downloads'))
+    main()
