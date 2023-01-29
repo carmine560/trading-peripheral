a7ec2d27e2a334b8314ea8d7e3ceb1f033b0b089
carmine <40491810+carmine560@users.noreply.github.com>
2023-01-29 14:44:44 +0900

    Add portfolio

--- watchlists.py
+++ watchlists.py
@@ -1,13 +1,15 @@
-import ast
-import configparser
 import os
 import sys
-import time
-
-from selenium.webdriver.common.by import By
 
 def main():
+    import ast
     import re
+
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
 
     import file_utilities
 
@@ -16,31 +18,30 @@ def main():
         os.path.basename(os.path.dirname(__file__)),
         os.path.splitext(os.path.basename(__file__))[0] + '.ini')
     config = configure(config_file)
+
+    if not config['Common']['portfolio']:
+        HYPERSBI2_ROOT = os.path.expandvars(
+            r'%APPDATA%\SBI Securities\HYPERSBI2')
+        identifier = ''
+        # TODO
+        for f in os.listdir(HYPERSBI2_ROOT):
+            if re.match('^[0-9a-z]{32}$', f):
+                identifier = f
+        if identifier:
+            config['Common']['portfolio'] = \
+                os.path.join(HYPERSBI2_ROOT, identifier, 'portfolio.json')
+        else:
+            print('unspecified identifier')
+            sys.exit(1)
+
     # TODO
     file_utilities.backup_file(config_file, number_of_backups=8)
     with open(config_file, 'w', encoding='utf-8') as f:
         config.write(f)
 
-    HYPERSBI2_ROOT = os.path.expandvars(r'%APPDATA%\SBI Securities\HYPERSBI2')
-    identifier = ''
-    # TODO
-    for f in os.listdir(HYPERSBI2_ROOT):
-        if re.match('^[0-9a-z]{32}$', f):
-            identifier = f
-    if identifier:
-        portfolio = os.path.join(HYPERSBI2_ROOT, identifier, 'portfolio.json')
-    else:
-        print('unspecified identifier')
-        sys.exit(1)
-
     file_utilities.backup_file(
-        portfolio, backup_root=config['Common']['portfolio_backup_root'])
-
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
+        config['Common']['portfolio'],
+        backup_root=config['Common']['portfolio_backup_root'])
 
     service = Service(executable_path=ChromeDriverManager().install(),
                       log_path=os.path.devnull)
@@ -55,10 +56,10 @@ def main():
     driver.implicitly_wait(1)
 
     action = ast.literal_eval(
-        config['Actions']['replace_sbi_securities_with_hypersbi2'])
+        config['Actions']['replace_sbi_securities'])
     interact_with_browser(driver, action)
 
-    watchlists = convert_to_yahoo_finance(config, portfolio)
+    watchlists = convert_to_yahoo_finance(config)
     for watchlist in watchlists:
         config['Common']['watchlist'] = watchlist
         action = ast.literal_eval(config['Actions']['export_to_yahoo_finance']
@@ -69,17 +70,19 @@ def main():
     driver.quit()
 
 def configure(config_file):
+    import configparser
+
     config = configparser.ConfigParser(
         interpolation=configparser.ExtendedInterpolation())
     config['Common'] = \
-        {'portfolio_backup_root': '',
-         'user_data_dir':
-         os.path.expandvars(r'%LOCALAPPDATA%\Google\Chrome\User Data'),
+        {'portfolio': '',
+         'portfolio_backup_root': '',
+         'user_data_dir': os.path.expandvars(r'%LOCALAPPDATA%\Google\Chrome\User Data'),
          'profile_directory': 'Default',
          'csv_root': os.path.join(os.path.expanduser('~'), 'Downloads'),
          'watchlist': ''}
     config['Actions'] = \
-        {'replace_sbi_securities_with_hypersbi2':
+        {'replace_sbi_securities':
          [('get', 'https://www.sbisec.co.jp/ETGate'),
           ('sleep', '1'),
           ('click', '//*[@id="new_login"]/form/p[2]/input'),
@@ -125,32 +128,36 @@ def configure(config_file):
     return config
 
 def interact_with_browser(driver, action):
+    import time
+
+    from selenium.webdriver.common.by import By
+
     for index in range(len(action)):
         command = action[index][0]
         if len(action[index]) > 1:
             argument = action[index][1]
 
-        if command == 'get':
-            driver.get(argument)
+        if command == 'clear':
+            driver.find_element(By.XPATH, argument).clear()
         elif command == 'click':
             driver.find_element(By.XPATH, argument).click()
-        elif command == 'clear':
-            driver.find_element(By.XPATH, argument).clear()
-        elif command == 'sleep':
-            time.sleep(int(argument))
         elif command == 'exist':
             if driver.find_elements(By.XPATH, argument):
                 interact_with_browser(driver, action[index][2])
-        elif command == 'send_keys':
-            driver.find_element(By.XPATH, argument).send_keys(action[index][2])
+        elif command == 'get':
+            driver.get(argument)
         elif command == 'refresh':
             driver.refresh()
+        elif command == 'send_keys':
+            driver.find_element(By.XPATH, argument).send_keys(action[index][2])
+        elif command == 'sleep':
+            time.sleep(int(argument))
 
-def convert_to_yahoo_finance(config, portfolio):
+def convert_to_yahoo_finance(config):
     import csv
     import json
 
-    with open(portfolio) as f:
+    with open(config['Common']['portfolio']) as f:
         dictionary = json.load(f)
 
     csv_root = config['Common']['csv_root']
