0071c9f79facaaf0daf20020214de7ca4e49ce0b
carmine <40491810+carmine560@users.noreply.github.com>
2023-01-30 19:06:37 +0900

    Add save_market_data

--- watchlists.py
+++ watchlists.py
@@ -34,40 +34,42 @@ def main():
             print('unspecified identifier')
             sys.exit(1)
 
-    # TODO
-    file_utilities.backup_file(config_file, number_of_backups=8)
-    with open(config_file, 'w', encoding='utf-8') as f:
-        config.write(f)
+    # # TODO
+    # file_utilities.backup_file(config_file, number_of_backups=8)
+    # with open(config_file, 'w', encoding='utf-8') as f:
+    #     config.write(f)
 
-    file_utilities.backup_file(
-        config['Common']['portfolio'],
-        backup_root=config['Common']['portfolio_backup_root'])
+    # file_utilities.backup_file(
+    #     config['Common']['portfolio'],
+    #     backup_root=config['Common']['portfolio_backup_root'])
 
-    service = Service(executable_path=ChromeDriverManager().install(),
-                      log_path=os.path.devnull)
-    options = Options()
-    options.add_argument('--headless=new')
-    options.add_argument('--user-data-dir='
-                         + config['Common']['user_data_dir'])
-    options.add_argument('--profile-directory='
-                         + config['Common']['profile_directory'])
-    driver = webdriver.Chrome(service=service, options=options)
-    # TODO
-    driver.implicitly_wait(1)
+    # service = Service(executable_path=ChromeDriverManager().install(),
+    #                   log_path=os.path.devnull)
+    # options = Options()
+    # options.add_argument('--headless=new')
+    # options.add_argument('--user-data-dir='
+    #                      + config['Common']['user_data_dir'])
+    # options.add_argument('--profile-directory='
+    #                      + config['Common']['profile_directory'])
+    # driver = webdriver.Chrome(service=service, options=options)
+    # # TODO
+    # driver.implicitly_wait(1)
 
-    action = ast.literal_eval(
-        config['Actions']['replace_sbi_securities'])
-    interact_with_browser(driver, action)
+    # action = ast.literal_eval(
+    #     config['Actions']['replace_sbi_securities'])
+    # interact_with_browser(driver, action)
 
-    watchlists = convert_to_yahoo_finance(config)
-    for watchlist in watchlists:
-        config['Common']['watchlist'] = watchlist
-        action = ast.literal_eval(config['Actions']['export_to_yahoo_finance']
-                                  .replace('\\', '\\\\')
-                                  .replace('\\\\\\\\', '\\\\'))
-        interact_with_browser(driver, action)
+    # watchlists = convert_to_yahoo_finance(config)
+    # for watchlist in watchlists:
+    #     config['Common']['watchlist'] = watchlist
+    #     action = ast.literal_eval(config['Actions']['export_to_yahoo_finance']
+    #                               .replace('\\', '\\\\')
+    #                               .replace('\\\\\\\\', '\\\\'))
+    #     interact_with_browser(driver, action)
 
-    driver.quit()
+    # driver.quit()
+
+    save_market_data(config)
 
 def configure(config_file):
     import configparser
@@ -81,6 +83,16 @@ def configure(config_file):
          'profile_directory': 'Default',
          'csv_root': os.path.join(os.path.expanduser('~'), 'Downloads'),
          'watchlist': ''}
+    config['Market Data'] = {
+        'opening_time': '9:20:00',
+        'closing_time': '15:50:00',
+        'time_zone': 'Asia/Tokyo',
+        'market_data_url': 'https://kabutan.jp/warning/?mode=2_9',
+        'number_of_pages': '2',
+        'symbol_header': 'コード',
+        'closing_price_header': '株価',
+        'closing_prices':
+        os.path.join('${Common:csv_root}', 'closing_prices_')}
     config['Actions'] = \
         {'replace_sbi_securities':
          [('get', 'https://www.sbisec.co.jp/ETGate'),
@@ -197,6 +209,94 @@ def convert_to_yahoo_finance(config):
         csv_file.close()
         watchlists.append(watchlist)
     return watchlists
+
+def save_market_data(config):
+    global pd
+    import pandas as pd
+
+    section = config['Market Data']
+    opening_time = section['opening_time']
+    closing_time = section['closing_time']
+    time_zone = section['time_zone']
+    market_data_url = section['market_data_url']
+    number_of_pages = section['number_of_pages']
+    symbol_header = section['symbol_header']
+    closing_price_header = section['closing_price_header']
+    closing_prices = section['closing_prices']
+
+    # market_data_url = 'https://kabutan.jp/warning/?mode=3_1'
+    number_of_pages = '1'
+    market_data_url = 'https://kabutan.jp/warning/?mode=3_1&market=0&capitalization=-1&stc=&stm=1&col=zenhiritsu'
+    market_data_url = 'https://kabutan.jp/warning/?mode=2_9&market=1'
+
+    # paths = []
+    # for i in range(1, 10):
+    #     paths.append(closing_prices + str(i) + '.csv')
+    # if get_latest(config, closing_time, time_zone, *paths,
+    #               volatile_time=opening_time):
+    if True:
+        dfs = []
+        for i in range(1, int(number_of_pages) + 1):
+            try:
+                dfs = dfs + pd.read_html(market_data_url + '&page=' + str(i),
+                                         match=symbol_header)
+            except Exception as e:
+                print(e)
+                sys.exit(1)
+
+        df = pd.concat(dfs)
+        # df.sort_values(by=symbol_header, inplace=True)
+        df = df.loc[df[closing_price_header] < 3000]
+
+        # for i in range(1, 10):
+        #     subset = df.loc[df[symbol_header].astype(str).str.match(
+        #         str(i) + '\d{3}5?$')]
+        #     subset.to_csv(closing_prices + str(i) + '.csv', header=False,
+        #                   index=False)
+
+        df.to_csv('00.csv', header=False, index=False)
+
+        df = df[[symbol_header]]
+        df = df.rename(columns={symbol_header: 'secCd'})
+
+        df.to_json('00.json', orient='records')
+        import json
+        asdf = json.loads(df.to_json(orient='records'))
+        for i in range(len(asdf)):
+            asdf[i]['marketCd'] = 'TKY'
+            asdf[i]['secKbn'] = 'ST'
+            asdf[i]['sortNo'] = i
+            asdf[i]['secCd'] = str(asdf[i]['secCd'])
+
+        import datetime
+        # datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
+        import re
+
+        watchlist = {
+            "listId": re.sub('\D', '', datetime.datetime.now().isoformat(timespec='milliseconds')),
+            "listName": "asdf",
+            "secCnt": len(asdf),
+            "sortNo": 0
+        }
+        watchlist['secList'] = asdf
+        # watchlist_json = json.dumps(watchlist)
+        # print(watchlist_json)
+
+        with open(config['Common']['portfolio']) as f:
+            dictionary = json.load(f)
+
+        # for i in range(len(dictionary['list'])):
+        #     print(dictionary['list'][i]['listName'])
+        # print([s for s in dictionary['list'] if s['listName'] != 'asdf'])
+        dictionary['list'] = [s for s in dictionary['list'] if s['listName'] != 'asdf']
+
+        dictionary['list'].append(watchlist)
+        # print(json.dumps(dictionary))
+
+        with open(config['Common']['portfolio'], 'w') as f:
+        #     dictionary = json.load(f)
+        # with open("sample.json", "w") as outfile:
+            json.dump(dictionary, f)
 
 if __name__ == '__main__':
     main()
