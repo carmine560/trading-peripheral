7286eb7c63d4998975731209d4127dc39c861a95
carmine <40491810+carmine560@users.noreply.github.com>
2023-11-02 12:27:38 +0900

    Fix services

--- trading_peripheral.py
+++ trading_peripheral.py
@@ -216,7 +216,7 @@ def configure(trade, can_interpolate=True, can_override=True):
         'last_inserted': '',
         'date_splitter': '<br>',
         'calendar_id': '',
-        'services': ('HYPER SBI 2', 'メインサイト'),
+        'services': ('HYPER SBI 2', 'HYPER SBI2', 'メインサイト'),
         'service_xpath':
         '//span[contains(@class, "font-xs font-bold") and text()="{0}"]',
         'function_xpath': 'ancestor::div[1]/p[1]',
